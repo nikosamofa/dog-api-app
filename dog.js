66ed24e22d907f3634f8fc941517652445011783
Nikos Amofa <nikosbright@gmail.com>
2020-10-20 03:20:47 -0400

    final JS commit

--- dog.js
+++ dog.js
@@ -1,3 +1,7 @@
+
+'use strict';
+
+
 //this function fetches three dog photos when called 
 function watchForm() {
     $('.js-dog-form').submit(event => {
@@ -6,36 +10,34 @@ function watchForm() {
         //this is the user input
         let submissions = $('.number-of-dogs').val();
 
-        //this make the image section visible    
-        $('.images').removeAttr('hidden');
-        displayResults(submissions);
-    })
+        //this make the image section visible        
+        getTheDog(submissions)
+    });
 };
-
 
 function getTheDog(submissions) {
     const url = `https://dog.ceo/api/breeds/image/random/${submissions}`;
     fetch(url)
         .then(response => response.json())
         .then(getDog => displayResults(getDog))
+
         .catch(error => alert('something went wrong, please try again'));
 };
 
 
 function displayResults(getDog) {
-    result = '';
-    for (let i = 0; i < getDog.length; i++) {
-        result += getDog[i]
+    $('.dog-images').html('');
+    for (let i = 0; i < getDog.message.length; i++) {
+        $('.dog-images').append(`<li><img src="${getDog.message[i]}" alt="picutre of a Dog" class="dogimages"/></li>`);
     };
-    $(".dog-images").closest(".dog-list").replaceWith(
-        `<li><input type="image" src="${result}" alt="picutre of a Dog" class="dog-images">
-                </li>`)
-}
+    $('.hidden-div').removeAttr('hidden')
+};
 
 
 function all() {
     console.log("The page has loaded")
     watchForm();
+    displayResults();
 };
 
 $(all);
